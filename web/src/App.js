9e99fc87c12f4063b4eeea53379fd3da77784a6c
lucas <teixeiralucas940@outlook.com>
2023-10-05 10:41:48 -0300

    Frontend
    Base for frontend using React, Axios and Styled-components.

---
+++ web/src/App.js
@@ -0,0 +1,17 @@
+import GlobalStyles from "./styles/GlobalStyles";
+
+function App() {
+  return (
+    <>
+      <div>
+      </div>
+      <div>
+        <h1>"Hello World!</h1>
+        <GlobalStyles />
+      </div>
+    </>
+
+  );
+}
+
+export default App;
