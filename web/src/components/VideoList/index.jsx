974b3cdaa7391751afafb095037e7a6295742f93
lucas <teixeiralucas940@outlook.com>
2023-10-19 10:01:41 -0300

    corrections with map

--- web/src/components/VideoList/index.jsx
+++ web/src/components/VideoList/index.jsx
@@ -11,7 +11,7 @@ import api from "../../services/api";
 export default function VideoList() {
     const [videos, setVideos] = useState([]);
 
-    useEffect(() => {
+    useEffect(() => { //captura no front os itens do BD
         api.get("videos").then(({ data }) => {
             setVideos(data.videos);
         });
@@ -23,12 +23,15 @@ export default function VideoList() {
     return (
         <Container>
             <VideoListWrapper>
-                <Video
-                    id={1}
-                    title="Title teste"
-                    link="https://www.youtube.com/watch?v=BnnbP7pCIvQ&list=RDBnnbP7pCIvQ&start_radio=1"
-                    liked={true}
-                />
+                {videos?.map((video) => ( //vai mapaear o objeto chamado pelo axios e exibir um video para cada item do BD
+                    <Video
+                        key={video._id}
+                        id={video._id}
+                        title={video.title}
+                        link={video.link}
+                        liked={video.liked}
+                    />
+                ))}
                 <AddVideo />
             </VideoListWrapper>
         </Container>
