f8d08f6ad0b78927660de277e57823efe39cc956
lucas <teixeiralucas940@outlook.com>
2023-10-19 09:21:08 -0300

    Axios
    Implementation of axios to consume api from database

--- web/src/components/VideoList/index.jsx
+++ web/src/components/VideoList/index.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 import { Container, VideoListWrapper } from "./styles";
+
 import AddVideo from "../AddVideo";
 import Video from "../Video";
 
+import api from "../../services/api";
+
 
 export default function VideoList() {
+    const [videos, setVideos] = useState([]);
+
+    useEffect(() => {
+        api.get("videos").then(({ data }) => {
+            setVideos(data.videos);
+        });
+        console.log(videos);
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
     return (
         <Container>
             <VideoListWrapper>
