2ddfed4817083631b028726dcf3e8f703dfe79f8
lucas <teixeiralucas940@outlook.com>
2023-10-27 12:38:52 -0300

    improve(response: adding SWR

--- frontend/src/components/VideoList/index.jsx
+++ frontend/src/components/VideoList/index.jsx
@@ -1,29 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-import { Container, VideoListWrapper } from "./styles";
-
+import { useAxios } from "../../hooks/useAxios";
 import AddVideo from "../AddVideo";
 import Video from "../Video";
 
-import api from "../../services/api";
-
+import { Container, VideoListWrapper } from "./styles";
 
 export default function VideoList() {
-    const [videos, setVideos] = useState([]);
-
-    useEffect(() => { //captura no front os itens do BD
-        api.get("videos").then(({ data }) => {
-            setVideos(data.videos);
-        });
-        console.log(videos);
-
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    const { data } = useAxios('videos');
 
     return (
         <Container>
             <VideoListWrapper>
-                {videos?.map((video) => ( //vai mapaear o objeto chamado pelo axios e exibir um video para cada item do BD
+                {data?.videos?.map((video) => ( //vai mapaear o objeto chamado pelo axios e exibir um video para cada item do BD
                     <Video
                         key={video._id}
                         id={video._id}
--- frontend/src/contexts/VideoContext.js
+++ frontend/src/contexts/VideoContext.js
@@ -2,10 +2,14 @@ import { createContext, useState } from "react";
 import FormModal from "../components/FormModal";
 
 import api from "../services/api";
+import { useAxios } from "../hooks/useAxios";
+
 
 export const VideoContext = createContext();
 
 export function VideoContextProvider({ children }) {
+    const { data, mutate } = useAxios('videos');
+
     const [openFormModal, setOpenFormModal] = useState(false);
     const [title, setTitle] = useState("");
     const [link, setLink] = useState("");
@@ -17,11 +21,10 @@ export function VideoContextProvider({ children }) {
 
     function handleClose() {
         if (title) {
-            setTitle("");
-
+            setTitle('');
         }
         if (link) {
-            setLink("");
+            setLink('');
         }
 
         setOpenFormModal(false);
@@ -38,24 +41,31 @@ export function VideoContextProvider({ children }) {
     function handleSubmit(event) {
         event.preventDefault();
 
-        if (title) {
-            setTitle("");
-
-        }
-        if (link) {
-            setLink("");
-        }
-
         const video = {
             title, link
         };
 
         if (id) {
             api.put(`videos/${id}`, video);
+
+            const updatedVideos = {
+                videos: data.videos?.map((video) => {
+                    if (video._id === id) {
+                        return { ...video, title, link };
+                    }
+                    return video;
+                }),
+            };
+            mutate(updatedVideos, false);
         }
 
         else {
             api.post('videos', video);
+
+            const updatedVideos = {
+                videos: [...data.videos, video]
+            }
+            mutate(updatedVideos, false);
         }
 
         setOpenFormModal(false);
@@ -63,10 +73,26 @@ export function VideoContextProvider({ children }) {
 
     function handleLike(id) {
         api.patch(`videos/${id}`)
+
+        const updatedVideos = {
+            videos: data.videos?.map((video) => {
+                if (video._id === id) {
+                    return { ...video, title: video.title, link: video.link, liked: !video.liked }
+                }
+                return video;
+            }),
+        };
+
+        mutate(updatedVideos, false);
     }
 
     function handleDelete(id) {
         api.delete(`videos/${id}`)
+
+        const updatedVideos = {
+            videos: data.videos?.filter((video) => video._id !== id),
+        }
+        mutate(updatedVideos, false);
     }
 
     function handleEdit(videoId, videoTitle, videoLink) {
